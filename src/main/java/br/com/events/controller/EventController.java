534f49179bb365aefa5cc86856052a67a959a28d
Paulo Ferreira <phgferreira@outlook.com>
2020-08-10 21:03:03 -0300

    Página de cadastro de eventos

---
+++ src/main/java/br/com/events/controller/EventController.java
@@ -0,0 +1,13 @@
+package br.com.events.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventController {
+
+	@RequestMapping("/registerEvent")
+	public String form() {
+		return "event/registerEvent";
+	}
+}
---
+++ src/main/java/br/com/events/model/Event.java
@@ -0,0 +1,45 @@
+package br.com.events.model;
+
+public class Event {
+
+	private String name;
+	private String place;
+	private String date;
+	private String hour;
+	
+	public Event() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getHour() {
+		return hour;
+	}
+
+	public void setHour(String hour) {
+		this.hour = hour;
+	}
+}
